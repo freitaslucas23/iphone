a16f68e5497a196a7c4c94ccd1729ecb63aeec2e
freitaslucas23 <83979713+freitaslucas23@users.noreply.github.com>
2024-07-02 12:59:59 -0300

    Add files via upload

---
+++ iphone/src/iPhone.java
@@ -0,0 +1,69 @@
+public class iPhone implements IReprodutorMusical, IAparelhoTelefonico, INavegadorInternet {
+
+    // Métodos da interface IReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música.");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    // Métodos da interface IAparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo a chamada.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz.");
+    }
+
+    // Métodos da interface INavegadorInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    public static void main(String[] args) {
+        iPhone meuIphone = new iPhone();
+
+        // Testando Reprodutor Musical
+        meuIphone.tocar();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+
+        // Testando Aparelho Telefônico
+        meuIphone.ligar("123456789");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando Navegador na Internet
+        meuIphone.exibirPagina("https://www.google.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
